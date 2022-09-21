a2db6de4f3cb4bb2112c12abdced48fbdadb2507
GuillermoTuesta <rrafterr44@gmail.com>
2022-09-21 14:41:26 +0200

    - AI.py additions, unfinished.

--- IT-Högskolan/AI-Labb2/AI.py
+++ IT-Högskolan/AI-Labb2/AI.py
@@ -8,9 +8,26 @@ import matplotlib.pyplot as plt
 import random as rand
 import numpy as np
 
-TestFile = open("TestFile.txt", "r")
-DataPoints = open("c:/OD/OneDrive - onmidroc/Documents/Github/Python-GuillermoTuesta/IT-Högskolan/AI-Labb2/datapoints.txt", "r") 
-TestPoints = open("c:/OD/OneDrive - onmidroc/Documents/Github/Python-GuillermoTuesta/IT-Högskolan/AI-Labb2/testpoints.txt", "r") # Had weird issues with python not finding the text files despite sharing folders with them.
+def Classify(TestPoint, MeasurementList, Amount=10):
+    ClosestPoints = {}
+    while len(ClosestPoints) < Amount:
+        SmallestDistance = 999999999999 # Huge number just to get the comparisons going.
+        for Measurement in MeasurementList:
+            Distance = (Measurement[0]**2 + Measurement[1]**2)**(1/2) 
+            if Distance < SmallestDistance:
+                SmallestDistance = Measurement
+        if SmallestDistance:
+            continue
+                
+
+                
+        
+    return None
+
+DataPoints = open("IT-Högskolan/AI-Labb2/datapoints.txt", "r") 
+TestPoints = open("IT-Högskolan/AI-Labb2/testpoints.txt", "r") 
+# Had weird issues with python not finding the text files despite sharing folders with them.
+# I'll just use the relative path.
 
 DataPoints.readline()
 TestPoints.readline() # Skipping the first lines before looping through them later on.
@@ -19,32 +36,38 @@ PikachuList = []
 PichuList = []
 
 for DataPoint in DataPoints:
-    DataPoint = DataPoint.split() # Turns text line into array of strings. This turns both PikachuList and PichuList into 2D arrays.
+    DataPoint = DataPoint.split() 
     if DataPoint[-1] == '0':
         PichuList.append(DataPoint)
         continue
     PikachuList.append(DataPoint)
+# Turns text line into array of strings. This turns both PikachuList and PichuList into 2D arrays.
 
-
-
-PikachuMeasurements = [[float(Measurement[0][:-1]),float(Measurement[1][:-1])] for Measurement in PikachuList]    # Clean up the strings, turn them into float numbers, and put them in their own arrays before inserting them.
-PichuMeasurements =   [[float(Measurement[0][:-1]),float(Measurement[1][:-1])] for Measurement in PichuList]      # So just 2D Arrays of the width and height measurements.
+PikachuData = [[float(Measurement[0][:-1]),float(Measurement[1][:-1]), int(Measurement[2])] for Measurement in PikachuList]    
+PichuData =   [[float(Measurement[0][:-1]),float(Measurement[1][:-1]), int(Measurement[2])] for Measurement in PichuList]     
+AllMeasurements = [[float(Measurement[0][:-1]),float(Measurement[1][:-1]), int(Measurement[2])] for Measurement in PikachuList] + [[float(Measurement[0][:-1]),float(Measurement[1][:-1]), int(Measurement[2])] for Measurement in PichuList] 
+# Clean up the strings, turn them into float numbers, and put them in their own arrays before inserting them.
+# They're now 2D Arrays of the width and height measurements.
 
 TestMeasurements = []
 for TestPoint in TestPoints:
     TestPoint = TestPoint.split()
-    TestMeasurement = [float(TestPoint[1][1:-1]),  float(TestPoint[2][:-1])]
+    TestMeasurement = [float(TestPoint[1][1:-1]),  float(TestPoint[2][:-1])] # Some string manipulation so I can convert to float.
     TestMeasurements.append(TestMeasurement)
 
-for Measurement in PikachuMeasurements:
-    plt.scatter(Measurement[0], Measurement[1], color="r") # Pikachu data points will be colored red.
+for Measurement in PikachuData:
+    plt.scatter(Measurement[0], Measurement[1], color="r") # Red for Pikachu.
 
-for Measurement in PichuMeasurements:
+for Measurement in PichuData:
     plt.scatter(Measurement[0], Measurement[1], color="b") # Blue for Pichu.
 
 for Measurement in TestMeasurements:
     plt.scatter(Measurement[0], Measurement[1], color='black') # Black for test points.
 
+plt.title("Scattered....")
+plt.annotate('Pikachu datapoint.',
+                xy=(PikachuData[0][0], PikachuData[0][1]),
+            )           
+plt.xlabel("Width")
+plt.ylabel("Height")
 plt.show()
-
-
--- IT-Högskolan/Intro AI22/LinjärRegressionRapport.md
+++ IT-Högskolan/Intro AI22/LinjärRegressionRapport.md
@@ -2,7 +2,7 @@
 Regression är när man anpassar en funktion till redan kända inmatnings- och utmatningsvärden. 
 En linjär regression är när man försöker hitta en linjär funktion som bäst passar det angivna värden.
 
-En linjär funktion kan matematiskt beskrivas som $ y = a + bx $, where a and b are the values to be tweaked until the function better fits the given data.
+En linjär funktion kan matematiskt beskrivas som $ y = a + bx $, where a and b are the values to be tweaked until the function better fits the given datapoints.
 
 
 
