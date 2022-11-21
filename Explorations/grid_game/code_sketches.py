d5f3c4a2f7444fe923c0a8d57b0c522ae6930b8b
GuillermoTuesta <rrafter44@gmail.com>
2022-11-21 23:48:28 +0100

    - Check Changelist for more details.

--- Explorations/grid_game/code_sketches.py
+++ Explorations/grid_game/code_sketches.py
@@ -51,19 +51,18 @@ class Inventory: # First draft of inventory descriptor class.
         self.inventory[resource_name] -= value
         return True
 
-@dataclass # Try dataclass instead maybe?
-class MapArray:
-    pass
-
 class MapArray: # First draft of MapArray class. Singleton?
     def __init__(self):
         self.map_array = None
         self.empty_grids = None
         self.valid_grids = None
+        self.current_seed = None
     
     @property
-    def map_array(self):
-        return self._map_array # Could be None. Otherwise should always hold the latest generated map_array.
+    def map_array(self) -> list: # Lazy property. If not set then it'll generate a new map before returning.
+        if self._map_array == None:
+            self.map_array = self.generate_map_array()
+        return self._map_array
 
     @map_array.setter
     def map_array(self, arr: list):
@@ -72,31 +71,88 @@ class MapArray: # First draft of MapArray class. Singleton?
         if len(arr) != 16*16:
             raise ValueError(f'map_array.setter: Given arr does not have the correct length. len(arr) = {len(arr)} ')
         self._map_array = arr
+        for y in range(16):
+            for x in range(16):
+                if (x,y) != arr[x + (y * 16)]:
+                    raise ValueError(f'MapArray Class map_array.setter, passed value cordarr[{x + (y * 16)}]')
 
     @map_array.deleter
     def map_array(self):
         self._map_array = None # Just turn it into None, don't use the setter.
 
-    def generate_map_array(self, seed=None): # Seeds will just save each grid's given number.
+    def generate_map_array(self, seed=None) -> list: # Seeds will just save each grid's given number.
         new_map_array = []
+        new_seed = []
+
         for y in range(16):
-            for x in range(16):
-                new_map_array.append([random.randrange(1, 23), (x, y)])
+            for x in range(16):  
+                grid_num = random.randrange(1,23) if not seed else seed[x + (y * 16)] # Grid_num is random unless a seed is given.
+                new_map_array.append([grid_num, (x, y)]) # Append random numbers to randomize grid type.
+                new_seed.append(grid_num) # If a seed was given, it'll save it again.
+
         self.map_array = new_map_array
+        self.current_seed = new_seed
 
     @property
-    def empty_grids(self):
+    def empty_grids(self): # Keeps track of current empty grids. Returns False if there is no generated map. Is also a lazy property.
         if self.map_array == None:
             return False
         if self.empty_grids == None:
             self._empty_grids = [arr[0] for arr in self.map_array if arr[0] in range (14)] # Return all grass grids (empty grids that can be spawned on)
         return self.empty_grids
 
+# https://www.codespeedy.com/__setitem__-and-__getitem__-in-python-with-example/
+# https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
+# Testing this out so it can be more easily handled.
+# Singleton class as well, which adds all new instances to it's own list without creating objects.
+# How to add interaction???
+
+class InteractFuncs: # Oooh... Maybe?
+    def __init__(self, func):
+        self.
 
 class Grids:
-    grid_list = []
-    def __init__(self):
-        
+    # Overriding magic methods to make it a singleton and handle a list of 'instances', and handles index behaviour.
+    def __new__(cls, grid_num, img, walkable, rect=None):
+        if not hasattr(cls, 'instance'): # Create new instance if one doesn't already exist. Otherwise, do not create a new one.
+            cls.instance = super(Grids, cls).__new__(cls)
+            cls.grid_list = [None]*64 # Default value. It will only hold 64 grids.
+        return cls.instance
+
+    def __setitem__(cls, index: int, map_arr: list, img: *PYGAME IMAGE OBJECT*, walkable: bool, rect: *PYGAME RECT OBJECT*, interact_func: function):
+        cls[index] = [map_arr[0], map_arr[1], img, walkable, rect] # Add [Grid_num, cords, image, walkable, rect]
+
+    def __getitem__(cls, index):
+        return cls.grid_list[index] # Return item from created grid_list.
+
+    @property
+    def cords(cls, index: int) -> list: # Dependant on the cord list given by the __new__ override.
+        return cls[index][1]
+
+    @property
+    def image(cls, index: int) -> *image object*: # Sort of lazy property. Gets corresponding image when it's actually asked for.
+        img_element = cls[index][2] # Get img element from grid_list. Could be a name in str format.
+        if img_element == str: # If it is a str, turn it into an image object.
+            cls[index][1] = pygame IMAGE TRANSFORM!!!!!
+        return img_element # Return the element.
+    
+    @property
+    def walkable(cls, index: int) -> bool:
+        return cls[index][3]
+    
+    @property
+    def rect(cls, index: int) -> *rect pygame object*: # Lazy property. Computes rect on demand.
+        rect_element = cls[index][4]
+        if rect_element == None:
+            cls[index][4] = pygame.Rect(rect_element[index][1][0] * unit_length, rect_element[index][1][0][1] * unit_length, unit_length, unit_length) # Get cords and actually make a rect.
+        return cls[index][4]
+
+    def render_all(cls):
+        for render_index in range(64):
+            screen.blit(cls[render_index].image, cls[render_index].rect)
+
+
+            
 
 
 
